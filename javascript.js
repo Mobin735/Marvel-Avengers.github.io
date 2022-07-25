1d395fabfc792086ecfa1ea0348a1a2de3b79bae
Mobin735 <83545765+Mobin735@users.noreply.github.com>
2022-07-26 00:33:27 +0530

    Add files via upload

---
+++ javascript.js
@@ -0,0 +1,37 @@
+let parallax = document.getElementById("main-bg");
+
+window.addEventListener("scroll", function () {
+    let value = window.scrollY;
+    parallax.style.top = value * 0.042 + 'px';
+})
+
+let vh= (window.innerHeight + 150) * 0.01;
+document.documentElement.style.setProperty('--vh', vh +'px');
+
+
+
+
+
+const g = document.querySelectorAll(".item");
+const videoPlayer = document.querySelector(
+    ".row .row_1 .youtube_video iframe"
+);
+
+const vqideo = (videoid, aactive) => {
+    console.log(aactive);
+    let videoUrl = `https://www.youtube.com/embed/${videoid}?rel=0`;
+    videoPlayer.setAttribute("src", videoUrl);
+    aactive.classList.add("active");
+
+};
+
+g.forEach((v) => {
+    v.addEventListener("click", () => {
+        // act.style.border = "0.2rem solid #393434";
+        // act.children[0].style.boxShadow = "none";
+        // act.children[0].style.borderRadius = "4px"
+        let ter = document.querySelector(".active");
+        ter.classList.remove("active");
+        vqideo(v.children[0].dataset.id, v);
+    });
+});
