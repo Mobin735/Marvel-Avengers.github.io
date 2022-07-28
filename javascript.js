4915d34a85e47cae2ca90a2e8c9d8fecb0754349
Mobin735 <83545765+Mobin735@users.noreply.github.com>
2022-07-28 18:21:35 +0530

    c

--- javascript.js
+++ javascript.js
@@ -2,23 +2,24 @@ let parallax = document.getElementById("main-bg");
 
 window.addEventListener("scroll", function () {
     let value = window.scrollY;
-    if (window.scrollY > 0) {
-        document.querySelector(".nav-blur").classList.remove("top");
-        let a = document.querySelector(".nav-bg");
-        a.style.backdropFilter = "blur(3.1px)";
-        a.style.backgroundColor = " rgb(49 46 46 / 13%)";
+    if (window.scrollY > 20) {
+        document.querySelector(".nav-blur,.nav-bg").classList.add("top");
+        
+        // let a = document.querySelector(".nav-bg");
+        // a.style.backdropFilter = "blur(3.1px)";
+        // a.style.backgroundColor = " rgb(49 46 46 / 13%)";
     }
     else {
-        document.querySelector(".nav-blur,.nav-bg").classList.add("top");
-        let a = document.querySelector(".nav-bg");
-        a.style.backdropFilter = "none";
-        a.style.backgroundColor = "transparent";
+        document.querySelector(".nav-blur").classList.remove("top");
+        // let a = document.querySelector(".nav-bg");
+        // a.style.backdropFilter = "none";
+        // a.style.backgroundColor = "transparent";
     }
 
-    parallax.style.top = value * 0.042 + 'px';
+    // parallax.style.top = value * 0.042 + 'px';
 })
 
-let vh = (window.innerHeight + 120) * 0.01;
+let vh = (window.innerHeight + 150) * 0.01;
 document.documentElement.style.setProperty('--vh', vh + 'px');
 
 
