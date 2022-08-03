d636ddbf37e46dfe855fc138e6651f639dcb24c9
Mobin735 <83545765+Mobin735@users.noreply.github.com>
2022-08-03 20:59:42 +0530

    new

--- javascript.js
+++ javascript.js
@@ -14,12 +14,14 @@ const hi = document.querySelector(".data-section");
 window.addEventListener("scroll", function () {
     document.querySelector(".nav-blur").classList.remove("menuon");
     let value = window.scrollY;
-    if (window.scrollY > 20) {
+    if (window.scrollY > 0) {
         document.querySelector(".nav-blur").classList.add("top");
+        document.querySelector(".nav-blur").style.position="fixed";
         // document.querySelector(".burger").classList.add("burger-top");
     }
     else {
         document.querySelector(".nav-blur").classList.remove("top");
+        document.querySelector(".nav-blur").style.position="relative";
         // document.querySelector(".burger").classList.remove("burger-top");
     }
     // parallax.style?.top = value * 0.042 + 'px';
